cb8c3d0840d44b91f9d81aa1eafd7773632987c3
Beyond Sky <beyond.sky@gmail.com>
2015-09-21 21:00:46 -0700

    remove comment

--- leetcode/java/GasStation_greedy.java
+++ leetcode/java/GasStation_greedy.java
@@ -1,17 +1,3 @@
-package cc150;
-
-/*
- * There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
-
-You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
-
-Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
-
-Note:
-The solution is guaranteed to be unique.
- 
- * 
- */
 public class GasStation_greedy {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         if (gas == null || cost == null || gas.length == 0 || cost.length == 0 || gas.length != cost.length)
